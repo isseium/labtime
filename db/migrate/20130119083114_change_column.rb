7dd84f966aa357b25fd5384cac00d1969681564d
whitech0c0 <hirotonbuu@gmail.com>
2013-01-19 17:47:35 +0900

    update

--- db/migrate/20130119083114_change_column.rb
+++ db/migrate/20130119083114_change_column.rb
@@ -1,6 +1,10 @@
 class ChangeColumn < ActiveRecord::Migration
   def change
-  	change_column :likes, :user_id, :integer
-  	change_column :likes, :work_id, :integer
+  	rename_column :likes, :user_id, :user_id_old
+  	rename_column :likes, :work_id, :work_id_old
+
+  	add_column :likes, :user_id, :integer
+  	add_column :likes, :work_id, :integer
+
   end
 end
