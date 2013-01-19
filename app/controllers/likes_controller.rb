643a2b63adfc7a046e3834ff5d4f3de2892ee5bf
whitech0c0 <hirotonbuu@gmail.com>
2013-01-19 17:05:24 +0900

    update

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,8 +2,8 @@
 class LikesController < ApplicationController
   def create
     like = Like.new
-    like.user_id = params[:user_id]
-    like.work_id = params[:work_id]
+    like.user_id = params[:user_id].to_i
+    like.work_id = params[:work_id].to_i
     like.save
     # if like.save
     #  redirect_to :back, :notice => "ファイトを送りました。"
